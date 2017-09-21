54b3b10544546267ca2e556c4f53ccfc85f0cea0
Fonix Hardcastle <me@fonix.online>
2017-09-21 20:37:35 +0100

    Some nice little updates! :D

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
-
 cd /home/container
 
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-  echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-
-  echo "╭━━━┳╮        ╭╮  ╭╮  ╭╮"
-  echo "┃╭━━┫┃        ┃╰╮╭╯┃ ╭╯┃"
-  echo "┃╰━━┫┃╭━━╮    ╰╮┃┃╭╯ ╰╮┃"
-  echo "┃╭━━┫┃┃╭╮┃╭━━╮ ┃╰╯┃   ┃┃"
-  echo "┃┃  ┃╰┫╰╯┃╰━━╯ ╰╮╭╯╭╮╭╯╰╮"
-  echo "╰╯  ╰━┻━━╯      ╰╯ ╰╯╰━━╯"
-  echo "Created By Fonix Hardcastle"
-  echo "  https://fonix.online"
-    sleep 4
+    echo ":/home/container$ ${MODIFIED_STARTUP}"
     
-#Run the needed setup prefs first!
-  echo "I am going to Connect to the Flo NFS system and check we have the files i need!"
-  echo ""
-  echo ""
-  sleep 3
-  sh start.sh
+#Run start.sh File!
+  echo "Running Flo's Preflight Checks!"
+    sleep 2
+      sh start.sh
 
 #Going to create an Alias for the NFS share given to us by the Flo System!
   echo "Setting up the Flo Backend Alias's to make sure eveyrthing runs nice and smooth!"
-  echo ""
-  echo ""
-  ln -s /mnt/maps /home/container/maps
-  sleep 1
-  echo "Hopefully you didnt see any errors from that!"
+    ln -s /mnt/maps /home/container/maps
+      sleep 1
+        echo "Hopefully you didnt see any errors from that!"
 
-# Run the Server
-  ${MODIFIED_STARTUP}
+# Run the Server 
+echo "Run the Minecraft Server Under Flo's Directives!"
+  sleep 1
+    ${MODIFIED_STARTUP}
 
   if [ $? -ne 0 ]; then
       echo "FONIX_CONTAINER_ERROR: There was an error while attempting to run the start command. Consult the All Mighty Fonix! Something went badly wrong!"
@@ -41,4 +27,4 @@ cd /home/container
     fi
 
 #Tell the User Flo has detected the Container Stopped
-  echo "Flo Has noticed the container has stopped, Hopefully you know this."
+  echo "Flo Has noticed the container has stopped, Hopefully you know this. GoodBye!"
