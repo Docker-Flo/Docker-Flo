0f91cef095dcd3f4ef8da55fc55d21d11c3a8a1d
Fonix Hardcastle <me@fonix.online>
2017-09-25 13:47:02 +0100

    Speed up the script now we know it works!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 cd /home/container
 
+#Talk about debug mode!
+echo "DEBUG MODE NON-ACTIVE!"
+
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
     echo ":/home/container$ ${MODIFIED_STARTUP}"
     
 #Run start.sh File!
-  echo "Running Flo's Preflight Checks!"
-    sleep 2
       sh start.sh
 
 #Going to create an Alias for the NFS share given to us by the Flo System!
-  echo "Setting up the Flo Backend Alias's to make sure eveyrthing runs nice and smooth!"
     ln -s /mnt/maps /home/container/maps
-      sleep 1
-        echo "Hopefully you didnt see any errors from that!"
 
 # Run the Server 
-echo "Run the Minecraft Server Under Flo's Directives!"
-  sleep 1
     ${MODIFIED_STARTUP}
 
   if [ $? -ne 0 ]; then
