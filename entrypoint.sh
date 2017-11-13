80cf5ccbcec8d60a78f10acf408d5f2962167244
Fonix Hardcastle <me@fonix.online>
2017-11-13 13:35:25 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd /home/container
 
-#Talk about debug mode!
-echo "DEBUG MODE NON-ACTIVE!"
+cd /home/container
 
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
@@ -18,12 +16,12 @@ echo "DEBUG MODE NON-ACTIVE!"
     ${MODIFIED_STARTUP}
 
   if [ $? -ne 0 ]; then
-      echo "FONIX_CONTAINER_ERROR"
+      echo "FLO_SCREAM"
       echo "Causes for this could include:"
       echo "      - No .jar file"
       echo "      - Container unable to be built"
       echo "      - Permissions are not in order"
-      echo "      - Container is un-bailed to be built"
+      echo "      - Docker has encountered an internal issue"
       echo "Please double check log files for more information!"
       sleep 1
       exit 1
@@ -31,9 +29,3 @@ echo "DEBUG MODE NON-ACTIVE!"
 
 #Tell the User Flo has detected the Container Stopped
   echo "Flo Has noticed the container has stopped, Hopefully you know this. GoodBye!"
-  
-#Copy over the Gradle information to the NFS share 
-   echo "Copying .gradle files to the NFS share"
-     mkdir /mnt/.gradle/servers/${SERVER_UUID}
-       cp -r /home/container/.gradle/* /mnt/.gradle/servers/${SERVER_UUID}
-       echo "Done! {CONTAINER STOPPED}
