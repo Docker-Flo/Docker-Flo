10f6c513e8fc5847fd7da14065ff9496d0e313bd
Fonix Hardcastle <me@fonix.online>
2017-11-27 04:01:22 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ cd /home/container
 
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-    echo "Setting up Mastermind Connections --> $SERVER_UUID"
     
 #Run start.sh File!
       sh start.sh
