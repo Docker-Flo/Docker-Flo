70b176342b11d22684a9dd7dafe22d68038ca1e8
Fonix Hardcastle <me@fonix.online>
2018-01-12 23:27:11 +0000

    Fix possible container error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,21 @@ cd /home/container
 
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-    
-#Run start.sh File!
-      sh start.sh
 
 #Going to create an Alias for the NFS share given to us by the Flo System! + Remove the permissions Errors
     ln -s /mnt /home/container/maps 2> /dev/null
+    
+  if [ -e /home/container/start.sh ]
+    then
+        echo "Start File Found!"
+        else
+          echo "WARNING START FILE DOES NOT EXIST = REBUILDING"
+          sleep 2
+          sh /home/container/maps/reroute.sh
+  fi
+    
+#Run start.sh File!
+    sh start.sh
       
 # Run the Server 
     ${MODIFIED_STARTUP}
