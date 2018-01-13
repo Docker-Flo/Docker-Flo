6cd7cc1c294c28c7e21e3f2e1b49e4007b5341d5
Fonix Hardcastle <me@fonix.online>
2018-01-13 04:01:06 +0000

    Small tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#Move to the container location
+# Move to the container location
 cd /home/container
 
 # Replace Startup Variables
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
-#Going to create an Alias for the NFS share given to us by the Flo System! + Remove the permissions Errors
+# Going to create an Alias for the NFS share given to us by the Flo System! + Remove the permissions Errors
     ln -s /mnt /home/container/maps 2> /dev/null
     
+# Check for the start script to fix start issues    
   if [ -e /home/container/start.sh ]
     then
         echo "Start File Found!"
@@ -18,12 +19,13 @@ cd /home/container
           sh /home/container/maps/reroute.sh
   fi
     
-#Run start.sh File!
+# Run pre-made flo retrive script File!
     sh start.sh
       
-# Run the Server 
+# Run the Server from Pterodactyl passthrough variables
     ${MODIFIED_STARTUP}
 
+# Server Start failure message
   if [ $? -ne 0 ]; then
       echo "FLO_SCREAM"
       echo "Causes for this could include:"
@@ -32,9 +34,8 @@ cd /home/container
       echo "      - Permissions are not in order"
       echo "      - Docker has encountered an internal issue"
       echo "      - The server is crashing too often"
-      echo "Please double check log files for more information!"
       exit 1
     fi
 
 #Tell the User Flo has detected the Container Stopped
-  echo "Flo Has noticed the container stopped, Hopefully you know this. GoodBye!"
+  echo "- = CONTAINER STOPPED = -"
