cfc78536eea2be3e963ee10b9cc71914af8ef938
Fonix Hardcastle <me@fonix.online>
2018-03-11 00:34:48 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ cd /home/container
   MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Going to create an Alias for the NFS share given to us by the Flo System! + Remove the permissions Errors
-  mkdir /home/container/maps
-    ln -s /mnt/ /home/container/maps 2> /dev/null
+  mkdir -p /home/container/maps
+    ln -s /mnt/ /home/container/maps
     
 # Check for the start script to fix start issues    
   if [ ! -f /home/container/start.sh ]
