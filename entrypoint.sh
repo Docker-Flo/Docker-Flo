d78639b6434cdbca80176c2291cd812e06c116aa
Fonix Hardcastle <me@fonix.online>
2018-03-24 15:13:21 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cd /home/container
 
 # Going to create an Alias for the NFS share given to us by the Flo System! + Remove the permissions Errors
   mkdir -p /home/container/maps
-    ln -s /mnt/ /home/container/maps
+    ln -s /mnt/ /home/container/maps/
     
 # Check for the start script to fix start issues    
   if [ ! -f /home/container/start.sh ]
